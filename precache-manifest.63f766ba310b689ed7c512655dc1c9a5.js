464771abfb7f0cd9abd7b0dea9678ae9fabf57bc
vadim-os <osinsky_vadim@yahoo.com>
2019-12-20 11:07:20 +0200

    Updates

---
+++ precache-manifest.63f766ba310b689ed7c512655dc1c9a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8212f3d17a7a9df64f113a93b8ee3b66",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "350f9059084e34055026",
+    "url": "/react_dynamic-list-of-todos/static/css/main.dd85c0b1.chunk.css"
+  },
+  {
+    "revision": "10fe1781dd2fab980336",
+    "url": "/react_dynamic-list-of-todos/static/js/2.4341eeb1.chunk.js"
+  },
+  {
+    "revision": "350f9059084e34055026",
+    "url": "/react_dynamic-list-of-todos/static/js/main.5814c1f3.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
